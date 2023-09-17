660dcf46bea858516872bea8175164b8cba05a69
Sebas <sebasherrera622@gmail.com>
2023-09-16 18:31:27 -0600

    Simplificacion de codigo

--- semaforo.c
+++ semaforo.c
@@ -7,18 +7,19 @@ int main(void)
 {
   sei(); // Se establece el bit I - Global Interrupt Enable
 
-  TCCR0B |= (1 << CS00 ); TCCR0B |= (1 << CS02 ) ; // CLK = CLK_IO[8 MHz]/1024 = 7812.5 HZ -> 1.28*10-4 s
+  TCCR0B |= (1 << CS00 ) | (1 << CS02 ) ; // CLK = CLK_IO[8 MHz]/1024 = 7812.5 HZ -> 1.28*10-4 s (Periodo del Reloj)
 
   TCCR0A |= (1 << WGM01 ) ; // Modo CTC 
 
-  //TCCR0A |= (1 << COM0A0 ) ; // Toggle OCOA on Compare Match (PIN B2)
-  //DDRB = 0x04; // Se establece el PIN B2 como Salida
+  OCR0A |= 0xF0; // Valor que se compara con TCNT0
 
-  OCR0A = 0xF0; // Valor que se compara con TCNT0
-
-  DDRB = 0x08; // Se establece el PIN B3 como Salida
+  DDRB |= (1 << DDB3 ) | (1 << DDB2 ) | (1 << DDB1 ) | (1 << DDB0 ); // Se establece el PIN B0, B1, B2 y B3 como Salida
 
   TIMSK |= (1 << OCIE0A ) ; // TIFR |= (1 << OCF0A  ) ; // Se habilita la interrupcion por comparacion con OCR0A    
+
+  MCUCR |= (1 << ISC01 ) | (1 << ISC11 ) ; //Se configura la interrupcion INT0 y INT1 por flanco decreciente
+
+  GIMSK |= (1 << INT1 ) | (1 << INT0 ) ; //Se habilita la interrupcion por INT0 y INT1
 
   int estado_LED = 0;
 
@@ -53,8 +54,23 @@ int main(void)
   }
 }
 
+
+
   // Logica Secuencial (Interrupciones)
   ISR ( TIMER0_COMPA_vect )
 {
   counter = counter + 1; 
 }
+
+
+/*
+  ISR ( INT0_vect )
+{
+  boton = 1; 
+}
+
+  ISR ( INT1_vect )
+{
+  boton = 1; 
+}
+*/
